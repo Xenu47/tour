05bab128692580e8d56e79d388987de158c3f075
Xenu47 <44733274+Xenu47@users.noreply.github.com>
2020-03-13 04:22:30 +0200

    pizdec
    Added +- normalnoe menu

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,48 @@
 <header>
-	<div id="header-menu-button" onclick="mouseOver()"></div>
+	<div id="header-menu-top">
+		<div id="header-menu-button" onclick="mouseOver()"></div>
 
-	<div id="header-logo">
-		<a href="/">
-			tourmix
-		</a>
+		<div id="header-logo">
+			<a href="/">
+				tourmix
+			</a>
+		</div>
 	</div>
 
 	<div style="display:none;" id="brightness_block" onclick="mouseOver()"></div>
-	<div id="header-menu">	
-		<div id="header-logo">
-			<a href="/">
-				<img src="images/logo.gif">
-			</a>
+	<div id="header-menu" class="closed">
+		<a href="/">
+			<img src="images/logo.gif" style="width:100%; height:auto;">
+		</a>
+		<a href="/tour" class="link">Поиск Туров</a>
+		<a href="/sightseeing" class="link">Экскурсии</a>
+		<a href="" class="link">Авиа, отели, круизы</a>
+		<a href="" class="link">Горящие туры</a>
+		<div class="inline-menu-name link" id="inline-menu-name-1" onclick="inlineMenu(this)">
+			<span>Страны</span>
+			<span class="dropdown-triangle"></span>
 		</div>
-		<a href="/tour">Поиск Туров</a>
-		<a href="/sightseeing">Экскурсии</a>
-		<a href="">Авиа, отели, круизы</a>
-		<a href="">Горящие туры</a>
-		<div id="inline-menu">Страны
-			<a href="">f</a>
-			<a href="">f</a>
-			<a href="">f</a>
-			<a href="">f</a>
+		<div class="inline-menu closed" id="inline-menu-1">
+			<a href="" class="link">Египет</a>
+			<a href="" class="link">Турция</a>
+			<a href="" class="link">Таиланд</a>
+			<a href="" class="link">Шри-Ланка</a>
+			<a href="" class="link">Индия</a>
+			<a href="" class="link">Испания</a>
+			<a href="" class="link">Кипр</a>
+			<a href="" class="link">Греция</a>
 		</div>
-		<div id="">О нас</div>
+		<div class="inline-menu-name link" id="inline-menu-name-2" onclick="inlineMenu(this)">
+			<span>О нас</span>
+			<span class="dropdown-triangle"></span>
+		</div>
+		<div class="inline-menu closed" id="inline-menu-2">
+			<a href="" class="link">Контакты и менеджеры</a>
+			<a href="" class="link">Отзывы туристов</a>
+			<a href="" class="link">Форма обратной связи</a>
+			<a href="" class="link">Cертификаты</a>
+		</div>
+		
 	</div>
 
 </header>
-
-
-<div style="display:none;" id="inv_val">0</div>
--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,51 +1,127 @@
 function mouseOver() {
+
+	var body = document.body,
+    	html = document.documentElement,
+    	header_menu = document.querySelector('#header-menu'),
+    	element_count = header_menu.childElementCount,
+    	element_height = header_menu.querySelector('#header-menu > a').clientHeight,
+    	header_height = document.querySelector("header").offsetHeight,
+    	menu_height = element_count*element_height;
+
+	var menu_state = header_menu.classList.item(0);
+
+	var height = Math.max( body.scrollHeight, body.offsetHeight, 
+		html.clientHeight, html.scrollHeight, html.offsetHeight );
+
 	// статус "в движении", тогда при повторном нажатии он не прекратит работу
-	var body = document.querySelector(body);
-	var header_menu = document.querySelector('#header-menu');
-	var element_count = header_menu.childElementCount;
-	var element_height = header_menu.querySelector('#header-menu > a').clientHeight;
-	var header_height = document.querySelector("header").offsetHeight;
-	var menu_height = element_count*element_height;
-	if(document.getElementById("inv_val").innerHTML == "1"){
+	if(menu_state == "moving"){
 	  return;
 	}
 	// статус "закрыт", задатся начальную позицию и скорость
-	if(document.getElementById("inv_val").innerHTML == "0"){
+	if(menu_state == "closed"){
+		header_menu.classList.toggle("closed");
+		window.scrollTo(0, 0);
 		var i = 0;
 		var step = 5 ;
 	}
 	// статус "открыт", задатся начальную позицию и скорость
-	if(document.getElementById("inv_val").innerHTML == "2"){
+	if(menu_state == "opened"){
+		header_menu.classList.toggle("opened");
 		var i = 70;
 		var step = -5;
 	}
 
-	document.getElementById("inv_val").innerHTML = "1";
+	header_menu.classList.toggle("moving");
 
 	var start = 0; // начало, 0 итераций
 	var timer = setInterval(function() {
 		start+=1; // +1 итерация
 		// если больше чем нужно - присваивается новый статус
-		if (start >= 15) {
+		if (start > 14) {
 			if(step == 5){
-				document.getElementById("inv_val").innerHTML = "2";
+				header_menu.classList.toggle("opened");
 			}
 			else{
-				document.getElementById("inv_val").innerHTML = "0";
+				header_menu.classList.toggle("closed");
 				document.querySelector("#brightness_block").style.display = 'none';
 				document.querySelector("#header-menu").style.display = 'none';
 			}
+			header_menu.classList.toggle("moving");
 			clearInterval(timer);
 			return;
 		}
 		i = i + step; // новый отступ от верха
-		dropdown(i); // применяется отсуп
+		dropdown(i, height); // применяется отсуп
 	}, 10);
 }
 
-function dropdown(i){
+function dropdown(i, height){
+	document.querySelector("#header-menu").style.height = height-50+"px";
 	document.querySelector("#header-menu").style.display = 'flex';
 	document.querySelector("#header-menu").style.left = 100-i+"%";
 	document.querySelector("#brightness_block").style.opacity = (i/100)
 	document.querySelector("#brightness_block").style.display = 'flex';
+}
+
+
+function inlineMenu(ele){
+	var inline_menu_number = ele.id.slice(-1);
+	var element_height = ele.clientHeight;
+	var inline_menu_name = "#inline-menu-"+inline_menu_number;
+	var inline_menu = document.querySelector(inline_menu_name);
+	var child_count = document.querySelectorAll(inline_menu_name+" a").length;
+	var inline_menu_state = inline_menu.classList.item(1);
+
+	var triangle = ele.childNodes[3];
+	var direction = 1;
+	var height = 0;
+
+	for (i = 1; i < child_count*2; i+=2) {
+		height+=inline_menu.childNodes[i].offsetHeight;
+	}
+
+	// статус "в движении", тогда при повторном нажатии он не прекратит работу
+	if(inline_menu_state == "moving"){
+	  	return;
+	}
+	// статус "закрыт", задатся начальную позицию и скорость
+	if(inline_menu_state == "closed"){
+		inline_menu.classList.toggle("closed");
+		direction = 1;
+		var i = 0;
+		var step = 10 ;
+	}
+	// статус "открыт", задатся начальную позицию и скорость
+	if(inline_menu_state == "opened"){
+		inline_menu.classList.toggle("opened");
+		direction = -1;
+		var i = 100;
+		var step = -10;
+	}
+
+	inline_menu.classList.toggle("moving");
+
+	var start = 0; // начало, 0 итераций
+	var timer = setInterval(function() {
+		start+=1; // +1 итерация
+		// если больше чем нужно - присваивается новый статус
+		if (start >= 11) {
+			if(step == 10){
+				inline_menu.classList.toggle("opened");
+			}
+			else{
+				inline_menu.classList.toggle("closed");
+			}
+			inline_menu.classList.toggle("moving");
+			clearInterval(timer);
+			return;
+		}
+		i = i + step; // новый отступ от верха
+		dropdownInline(i, height, inline_menu, triangle, direction); // применяется отсуп
+	}, 10);
+}
+
+function dropdownInline(i, height, inline_menu, triangle, direction){
+	inline_menu.style.height = height*i*0.01;
+	triangle.style.transform = "rotate("+(i*1.80*direction + 180)+"deg)";
 }
