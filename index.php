1cf543c6c673b5e9b47834b93f3b4076615db597
Xenu47 <gelun100@gmail.com>
2021-01-24 23:51:53 +0200

    wtf

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-  include_once('pages/main.php');
+	session_start();
+
+	// Set Language variable
+	if(isset($_GET['lang']) && !empty($_GET['lang'])){
+		$_SESSION['lang'] = $_GET['lang'];
+
+		if(isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']){
+			echo "<script type='text/javascript'> location.reload(); </script>";
+		}
+	}
+
+	// Include Language file
+	if(!isset($_SESSION['lang'])){
+		$_SESSION['lang'] = 'ua'; // 'ua|ru'
+	}
+	include("lang/lang_".$_SESSION['lang'].".php");
+	include_once('pages/main.php');
 ?>
